204ac4c10f2e8d11927bb3a74f2e7d5ce21125dd
adam morgan <adammorgan1702@gmail.com>
2025-01-10 16:11:03 +0000

    enemies spawned at sensible distribution

--- scripts/Spawner.cs
+++ scripts/Spawner.cs
@@ -1,11 +1,15 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Spawner : Node2D
 {
 	[Export] public PackedScene EnemyScene { get; set; }
 	[Export] public int SpawnCount { get; set; } = 5;
 	[Export] public Rect2 SpawnArea { get; set; } = new Rect2(Vector2.Zero, new Vector2(400, 400));
+	[Export] public float MinSpawnDistance {get; set;} = 400f;
+	
+	private List<Vector2> _spawnedPositions = new List<Vector2>();
 
 	public override void _Ready()
 	{
@@ -18,18 +22,33 @@ public partial class Spawner : Node2D
 
 		for (int i = 0; i < SpawnCount; i++)
 		{
+			int attempt = 0;
+			int maxAttempt = 10;
+			Vector2 spawnPosition;
 			// Instance the enemy
 			Node2D enemy = (Node2D)EnemyScene.Instantiate();
-
-			// Randomize the spawn position within the defined area
-			Vector2 spawnPosition = SpawnArea.Position + new Vector2(
-				(float)random.NextDouble() * SpawnArea.Size.X,
-				(float)random.NextDouble() * SpawnArea.Size.Y
-			);
+			do {
+				// Randomize the spawn position within the defined area
+				spawnPosition = SpawnArea.Position + new Vector2(
+					(float)random.NextDouble() * SpawnArea.Size.X,
+					(float)random.NextDouble() * SpawnArea.Size.Y
+				);
+				attempt++;
+			} while(IsTooClose(spawnPosition) && attempt < maxAttempt);
 			enemy.Position = spawnPosition;
 
+			_spawnedPositions.Add(spawnPosition);
 			// Add the enemy to the scene
 			AddChild(enemy);
 		}
+	}
+	
+	private bool IsTooClose(Vector2 position) {
+		foreach (var otherPosition in _spawnedPositions){
+			if(position.DistanceTo(otherPosition) > MinSpawnDistance){
+				return true;
+			}
+		}
+		return false;
 	}
 }
