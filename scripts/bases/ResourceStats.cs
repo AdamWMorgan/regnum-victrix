0c826e96daf5cd69f0ad4b5a5f507b94a64625da
adam morgan <adammorgan1702@gmail.com>
2025-05-13 12:30:29 +0100

    [rv-2] adding all resources to stat display

--- scripts/bases/ResourceStats.cs
+++ scripts/bases/ResourceStats.cs
@@ -3,29 +3,73 @@ using System;
 
 public partial class ResourceStats : RichTextLabel
 {
-	private Node2D attachedBase;
-	// Called when the node enters the scene tree for the first time.
+	private double REFRESH_RATE = 5.0;
+	private double _timeAccumulator = 0.0;
+
 	public override void _Ready()
-	{		
+	{
 		CallDeferred(nameof(InitialiseValues));
-		// Enable BBCode parsing so [img] tags work
-		BbcodeEnabled  = true;
+		BbcodeEnabled = true;
 		FitContent = true;
 	}
 
 	public override void _Process(double delta)
 	{
-		Clear();
-		InitialiseValues();
+		_timeAccumulator += delta;
+
+		if (_timeAccumulator >= REFRESH_RATE)
+		{
+			_timeAccumulator = 0;
+			Clear();
+			InitialiseValues();
+		}
 	}
 
-
-	private void InitialiseValues(){
+	private void InitialiseValues()
+	{
 		Node attachedBase = GetParent()?.GetParent();
 
-		if(attachedBase is IBaseProvider provider){
-			GD.Print();
-			AppendText("[img]res://game/assets/materials/gold.png[/img] " + provider.GetAttachedBase().Resources.Find(resource => resource.Type == ResourceType.GOLD).Quantity);
+		if (attachedBase is IBaseProvider provider)
+		{
+			var gold = provider.GetAttachedBase().Resources
+				.Find(resource => resource.Type == ResourceType.GOLD);
+
+			var water = provider.GetAttachedBase().Resources
+				.Find(resource => resource.Type == ResourceType.WATER);
+
+			var wheat = provider.GetAttachedBase().Resources
+				.Find(resource => resource.Type == ResourceType.WHEAT);
+
+			var wood = provider.GetAttachedBase().Resources
+				.Find(resource => resource.Type == ResourceType.WOOD);
+
+			var iron = provider.GetAttachedBase().Resources
+				.Find(resource => resource.Type == ResourceType.IRON);
+
+			if (gold != null)
+			{
+				AppendText($"[img]res://game/assets/materials/gold.png[/img] {gold.Quantity}");
+			}
+
+			if (water != null)
+			{
+				AppendText($"[img]res://game/assets/materials/water.png[/img] {water.Quantity}");
+			}
+
+			if (wheat != null)
+			{
+				AppendText($"[img]res://game/assets/materials/wheat.png[/img] {wheat.Quantity}");
+			}
+
+			if (wood != null)
+			{
+				AppendText($"[img]res://game/assets/materials/wood.png[/img] {wood.Quantity}");
+			}
+
+			if (iron != null)
+			{
+				AppendText($"[img]res://game/assets/materials/iron.png[/img] {iron.Quantity}");
+			}
 		}
 	}
 }
