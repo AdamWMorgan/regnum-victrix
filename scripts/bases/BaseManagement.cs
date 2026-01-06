65927caee8273f8905dd996c913ad0612d64511d
adam morgan <adammorgan1702@gmail.com>
2026-01-06 20:41:06 +0000

    [rv-5] switching to use containers for upgrade buttons

--- scripts/bases/BaseManagement.cs
+++ scripts/bases/BaseManagement.cs
@@ -1,21 +1,39 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class BaseManagement : Control
 {
-	public String BUTTON_PATH = "BaseManagementPanel/VBoxContainer/";
+	public string BUTTON_PATH = "BaseManagementPanel/VBoxContainer/";
 	public Base _base;
 	public Button baseUpgradeBtn;
 	public Button troopUpgradeBtn;
+	
+	public PanelContainer troopUpgradeContainer;
+	public RichTextLabel baseUpgradeBtnTxt;
+	public RichTextLabel troopUpgradeBtnTxt;
 
 	public override void _Ready()
 	{
 		_base = GetParent<Base>();
-		baseUpgradeBtn = GetNode<Button>( BUTTON_PATH + "BaseUpgradeBtn");
-		troopUpgradeBtn = GetNode<Button>(BUTTON_PATH + "TroopUpgradeBtn");
+		
 
-		baseUpgradeBtn.Text = "base upgrade values placeholder";
-		troopUpgradeBtn.Text = "base upgrade values placeholder";
+		// need to add all of this logic for base upgrade, as well as looping through the values to build up
+		// the hbox with the prices. This price definition will need to come from some higher level rule conifg.
+		troopUpgradeContainer = GetNode<PanelContainer>(BUTTON_PATH + "TroopUpgradeContainer");
+
+		HBoxContainer hBox = new HBoxContainer();
+
+		var icon = new TextureRect();
+		icon.Texture = GD.Load<Texture2D>("res://game/assets/materials/wheat.png");
+
+		var label = new Label();
+		label.Text = "wheat";
+
+		hBox.AddChild(icon);
+		hBox.AddChild(label);
+
+		troopUpgradeContainer.AddChild(hBox);
 	}
 
 	public override void _Process(double delta)
