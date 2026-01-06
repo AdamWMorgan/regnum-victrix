9b094fa675202e9cdd419f868339d9ef22578939
adam morgan <adammorgan1702@gmail.com>
2026-01-06 20:53:53 +0000

    [rv-5] adding an additonal continaer for base upgrade button

--- scripts/bases/BaseManagement.cs
+++ scripts/bases/BaseManagement.cs
@@ -6,34 +6,43 @@ public partial class BaseManagement : Control
 {
 	public string BUTTON_PATH = "BaseManagementPanel/VBoxContainer/";
 	public Base _base;
-	public Button baseUpgradeBtn;
-	public Button troopUpgradeBtn;
+	public PanelContainer baseUpgradeContainer;
 	
 	public PanelContainer troopUpgradeContainer;
-	public RichTextLabel baseUpgradeBtnTxt;
-	public RichTextLabel troopUpgradeBtnTxt;
 
 	public override void _Ready()
 	{
 		_base = GetParent<Base>();
 		
+		baseUpgradeContainer = GetNode<PanelContainer>(BUTTON_PATH + "BaseUpgradeContainer");
 
-		// need to add all of this logic for base upgrade, as well as looping through the values to build up
-		// the hbox with the prices. This price definition will need to come from some higher level rule conifg.
-		troopUpgradeContainer = GetNode<PanelContainer>(BUTTON_PATH + "TroopUpgradeContainer");
-
-		HBoxContainer hBox = new HBoxContainer();
+		HBoxContainer baseHBox = new HBoxContainer();
 
 		var icon = new TextureRect();
 		icon.Texture = GD.Load<Texture2D>("res://game/assets/materials/wheat.png");
 
 		var label = new Label();
-		label.Text = "wheat";
+		label.Text = "100";
 
-		hBox.AddChild(icon);
-		hBox.AddChild(label);
+		baseHBox.AddChild(icon);
+		baseHBox.AddChild(label);
 
-		troopUpgradeContainer.AddChild(hBox);
+		baseUpgradeContainer.AddChild(baseHBox);
+
+		// need to add all of this logic for base upgrade, as well as looping through the values to build up
+		// the hbox with the prices. This price definition will need to come from some higher level rule conifg.
+		troopUpgradeContainer = GetNode<PanelContainer>(BUTTON_PATH + "TroopUpgradeContainer");
+
+		HBoxContainer troopHBox = new HBoxContainer();
+		var icon2 = new TextureRect();
+		icon2.Texture = GD.Load<Texture2D>("res://game/assets/materials/wheat.png");
+
+		var label2 = new Label();
+		label2.Text = "300";
+		troopHBox.AddChild(icon2);
+		troopHBox.AddChild(label2);
+
+		troopUpgradeContainer.AddChild(troopHBox);
 	}
 
 	public override void _Process(double delta)
