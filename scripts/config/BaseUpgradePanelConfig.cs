95d8c52e8b36975a127c615fb5ccdf5ecc8be9a3
adam morgan <adammorgan1702@gmail.com>
2026-01-08 23:02:56 +0000

    [rv-5] adding max level config value

--- scripts/config/BaseUpgradePanelConfig.cs
+++ scripts/config/BaseUpgradePanelConfig.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 
 public class BaseLevelConfigPanel {
     public float Multiplier { get; set; }
+    public float Max { get; set; }
     public List<ResourceCost> TroopUpgrade { get; set; }
     public List<ResourceCost> BaseUpgrade { get; set; }
 }
